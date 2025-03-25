1fdf21f787cb93fe5e265f43cfacf3cfed1c1c6f
Rennan <141781304+Eorennas@users.noreply.github.com>
2025-03-25 20:37:25 -0300

    .

--- projeto-front-end/src/component/Menu-Lateral.tsx
+++ projeto-front-end/src/component/Menu-Lateral.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { FaHome, FaBars } from 'react-icons/fa';
+import { useNavigate } from "react-router-dom";
 
 export default function Menu() {
+  const navigate = useNavigate();
   return (
     <div className="z-0 w-16 h-screen bg-gradient-to-b from-blue-100 to-blue-300 flex flex-col items-center shadow-md fixed top-0 left-0">
       <div className="mt-4 p-2 text-gray-700 hover:text-blue-500 cursor-pointer">
         <FaBars size={24} />
       </div>
       <div className="mt-8 p-2 text-gray-700 hover:text-blue-500 cursor-pointer">
-        <button><FaHome size={24} /></button>
+        <button onClick={() => navigate('/')}><FaHome size={24} /></button>
       </div>
     </div>
   );
